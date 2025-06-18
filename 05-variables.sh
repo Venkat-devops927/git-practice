eb774b29d3d46410eb38cda93ac4d9aff2642309
Venkat-devops927 <venkataryan927@gmail.com>
2025-06-19 00:13:53 +0530

    variable

--- 05-variables.sh
+++ 05-variables.sh
@@ -4,3 +4,7 @@ echo "please enter your username::"
 
 read -s USERNAME # takes inputs into USERNAME variable 
 echo "your username is $USERNAME"
+ 
+ echo "please enter your password"
+  read -s PASSWORD
+  echo "your enterd password is $PASSWORD"

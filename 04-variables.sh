30877803ba8d3b632d6d7dc4ff83aaac441f81ad
Venkat-devops927 <venkataryan927@gmail.com>
2025-06-19 00:01:55 +0530

    variables

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: hello $PERSON2. how are you"
+echo "$PERSON2:: hi $PERSON1.iam good wt about you"
+echo "$PERSON1:: iam learning devops my own $PERSON2"
+echo "$PERSON2::oh its great $PERSON1 keep it up"

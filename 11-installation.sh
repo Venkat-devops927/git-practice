4a319651694c54f213ce095d5b42e0a1c1259757
Venkat-devops927 <venkataryan927@gmail.com>
2025-07-22 22:06:53 +0530

    conditions

--- 11-installation.sh
+++ 11-installation.sh
@@ -7,9 +7,15 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "please run this script with root previlizes"
-    
+    exit 1
 
 fi
 
-
-dnf install git -y
+dnf list installed git
+ 
+if [ $? -ne 0 ]
+then  echo "git is not installed going to install it"
+    dnf install git -y
+else 
+    echo"git is already installed ,nothing to do it"
+fi

5da6aa4bdc0c48147a0b458d5bc0dfba7856d778
Venkat-devops927 <venkataryan927@gmail.com>
2025-07-22 21:39:10 +0530

    conditions

--- 11-installation.sh
+++ 11-installation.sh
@@ -1,4 +1,11 @@
 #!/bin/bash/
 
 USERID=$(id -u)
-echo "user id is :$USERID"
+#echo "user id is :$USERID"
+
+
+if [ $USERID -ne 0]
+then
+    echo "please run this script with root previlizes"
+
+fi

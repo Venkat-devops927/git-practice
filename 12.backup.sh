ad24374e79869c775d6d445bfff9a2bf607f74a4
Venkat-devops927 <venkataryan927@gmail.com>
2025-12-12 08:55:59 +0530

    shell-practice

---
+++ 12.backup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14} #if $3 is empty then defult is 14 days
+R="\e[31m" 
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+# check the source and des dir  are provided
+
+USAGE(){
+    echo -e  "$R USAGE:: $N sh 12.backup.sh <source> <dest> <days> #days optional"
+
+
+if [ $# -lt 2 ]
+then
+    echo ""
